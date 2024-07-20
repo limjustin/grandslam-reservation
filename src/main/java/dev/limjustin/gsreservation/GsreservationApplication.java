d3cecf0c7d97cdd9c96135f75e7f64e264d9439c
Jaeyoung Lim <limjyjustin@gmail.com>
2024-07-20 14:04:46 +0900

    initial commit

---
+++ src/main/java/dev/limjustin/gsreservation/GsreservationApplication.java
@@ -0,0 +1,13 @@
+package dev.limjustin.gsreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/limjustin/gsreservation/GsreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limjustin.gsreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
