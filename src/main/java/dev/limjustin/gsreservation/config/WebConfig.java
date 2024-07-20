8f8e80d6d5120c159fc8eb780fe95f7efe2a3c16
Jaeyoung Lim <limjyjustin@gmail.com>
2024-07-21 01:02:06 +0900

    add: cors 코드 추가

---
+++ src/main/java/dev/limjustin/gsreservation/config/WebConfig.java
@@ -0,0 +1,17 @@
+package dev.limjustin.gsreservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
+    }
+}
